9efd1b2f071fc3e530a66a13959895851df17b1b
Jhon Yepe <jhonmigue.1@hotmail.com>
2022-11-22 11:45:07 -0400

    omaigat

--- source/api/actions/votes.js
+++ source/api/actions/votes.js
@@ -1,20 +1,64 @@
+const fields = require('anxeb-mongoose/source/middleware/fields');
 const anxeb = require('anxeb-node');
 
 module.exports = {
-	url     : '/votes/',
-	access  : anxeb.Route.access.public,
-	timeout : 60000,
-	methods : {
-        get : function (context) {},
-        post : function (context) {
-           const Data= context.payload;
-           if (!Data.Matricula || !Data.Curso || !Data.Votacion) {
-               ccontext.log.exception.invalid_request.throw();
-               context.send.badRequest();
-               return;
-           }
-           //insert it on mongo db
+    url: '/vote',
+    access: anxeb.Route.access.public,
+    timeout: 60000,
+    methods: {
+        get: function (context) {
            
         },
-	}
+        post: async function (context) {
+            const Data = context.payload.Student;
+           VerifyData(context, Data);
+            //insert it on mongo db
+            let student = await context.data.upsert.Student();
+            student.Matricula = Data.Matricula;
+            student.Curso = Data.Curso;
+            student.Votes = Data.Votes;
+            student.Votes.Fecha = anxeb.utils.date.utc().unix();
+            await context.data.validate({
+                model  : student
+            });
+            await student.persist();
+            context.send(student.toClient());
+        },
+    }
 };
+async function VerifyData(context, data){
+    //veify structure
+    if (!Data.Matricula || !Data.Curso || !Data.Votes) {
+        context.log.exception.invalid_request.throw();
+    }
+    //verify if the Matricula is well formated 2019-0000 (the first 4 digits can only be  2019)
+    if (!Data.Matricula.match(/2019-\d{4}/)) {
+        context.log.exception.invalid_request.args( {'Matricula': Data.Matricula}).include({
+            fields: {msg: 'Matricula must be in the format 2019-0000',
+            name: 'Matricula', index: 1, Matricula: Data.Matricula}
+
+        }).throw();
+    }
+    //veify if the student exist
+    const isRegitered = await context.data.find.Student({ 'Matricula': Data.Matricula });
+    if (isRegitered){
+        context.log.exception.selected_name_unavailable.args('Matricula').include(
+           { fields: [{name: 'Matricula', index: 1, Matricula: Data.Matricula, 
+           msg: 'This Matricula has voted',}]
+    }).throw();
+    }
+    //verify if the course exist A-G
+    const LCurso =['A','B','C','D','E','F','G'];
+    if (!LCurso.includes(Data.Curso,Seccion)){
+        context.log.exception.invalid_request.args('Curso').include(
+           { fields: [{name: 'Curso.Seccion', index: 1, Curso: Data.Curso.Seccion }]
+    }).throw();
+    }
+    //verify if the vote is well formated
+    //check if the vote is a number
+    if (isNaN(Data.Votes.Voto)){
+        context.log.exception.invalid_request.args('Voto').include(
+           { fields: [{name: 'Voto', index: 1, Voto: Data.Votes.Voto }]
+    }).throw();
+    }
+}
